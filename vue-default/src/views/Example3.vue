af5d0f20392aa407efafc87561ec690737f5aaa6
zeemoong <ljw722007@gmail.com>
2021-11-02 15:15:45 +0900

    vue.json 에 사용자 템플릿 저장

---
+++ vue-default/src/views/Example3.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
